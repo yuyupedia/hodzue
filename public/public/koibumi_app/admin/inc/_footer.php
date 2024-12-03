2ce0e20759cec057ed3e314791b8425971649850
yuyupedia <52689517+yuyupedia@users.noreply.github.com>
2024-12-04 02:43:41 +0900

    archived sizu.me

--- public/public/koibumi_app/admin/inc/_footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <p><a href="https://do.gt-gt.org/" target="_blank">コイブミ（Ver1.3.1）</a></p>
-</footer>
-
-</div>
-
-<script src="func.js"></script>
-</body>
-</html>
--- public/public/newiine_app/admin/inc/_footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <p><a href="https://do.gt-gt.org/" target="_blank">いいねボタン改（Ver0.1.0）</a></p>
-</footer>
-
-</div>
-
-<script src="func.js"></script>
-</body>
-</html>
